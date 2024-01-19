7a1bcc124648d92d468b92a93f3177b24b7d9d25
Christian Manzaraz <magnusmanz@gmail.com>
2024-01-19 12:51:39 -0300

    Ch. 8.2 Firebase Email & Password Signup & Login in SwiftUI (Snacktacular App)

---
+++ Snacktacular/LoginView.swift
@@ -0,0 +1,100 @@
+//
+//  ContentView.swift
+//  Snacktacular
+//
+//  Created by Christian Manzaraz on 19/01/2024.
+//
+
+import SwiftUI
+import Firebase
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var showingAlert = false
+    @State private var alertMessage = ""
+    
+    var body: some View {
+        NavigationStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            Group {
+                TextField("E-mail", text: $email)
+                    .keyboardType(.emailAddress)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .submitLabel(.next)
+                
+                SecureField("Password", text: $password)
+                    .textInputAutocapitalization(.never)
+                    .submitLabel(.done)
+                
+            }
+            .textFieldStyle(.roundedBorder)
+            .overlay {
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(.gray.opacity(0.5), lineWidth: 2)
+            }
+            .padding(.horizontal)
+            
+            HStack {
+                Button {
+                    register()
+                } label: {
+                    Text("Sign Up")
+                }
+                .padding(.trailing)
+                
+                Button {
+                    login()
+                } label: {
+                    Text("Log In")
+                }
+                .padding(.leading)
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(Color("SnackColor"))
+            .font(.title2)
+            .padding(.top)
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .alert(alertMessage, isPresented: $showingAlert) {
+            Button("OK", role: .cancel) {}
+        }
+    }
+    
+    func register() {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {// login error occurred
+                print("😡SIGN-IN ERROR: \(error.localizedDescription)")
+                alertMessage = "😡LOGIN ERROR: \(error.localizedDescription)"
+                showingAlert = true
+            } else {
+                print("😎 Registration Success!")
+                // TODO: Load ListView
+            }
+            
+            
+        }
+    }
+    
+    func login() {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("😡LOGIN ERROR: \(error.localizedDescription)")
+                alertMessage = "😡SIGNUP ERROR: \(error.localizedDescription)"
+                showingAlert = true
+            } else {
+                print("😎 Login SuccessFULL!")
+                // TODO: Load ListView
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
