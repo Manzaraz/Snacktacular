3ce1172211737a0c109b726edb7e65e28d93e900
Christian Manzaraz <magnusmanz@gmail.com>
2024-01-19 10:57:18 -0300

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Christian Manzaraz on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
