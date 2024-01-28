a8a51f24a4d3aad5238863179ea4e8f01382d5a1
Christian Manzaraz <magnusmanz@gmail.com>
2024-01-27 21:30:01 -0300

    Ch. 8.17 Part 1: Saving Images in Cloud Storage for Firebase using SwiftUI

---
+++ Snacktacular/Models/Photo.swift
@@ -0,0 +1,22 @@
+//
+//  Photo.swift
+//  Snacktacular
+//
+//  Created by Christian Manzaraz on 27/01/2024.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct Photo: Identifiable, Codable {
+    @DocumentID var id: String?
+    var imageURLString = "" // This will hold the URL for loading the image
+    var description = ""
+    var reviewer = Auth.auth().currentUser?.email ?? ""
+    var postedOn = Date()
+    
+    var dictionary: [String: Any] {
+        return ["imageURLString": imageURLString, "description": description, "reviewer": reviewer, "postedOn": Timestamp(date: Date())]
+    }    
+}
